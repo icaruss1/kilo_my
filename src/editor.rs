e9217129c0ee03f6d8c62853100f7d36e08e6df5
icaruss1 <icarussyed@gmail.com>
2023-09-28 17:41:46 +0200

    refactored into terminal struct tracking screen state

--- src/editor.rs
+++ src/editor.rs
@@ -1,45 +1,18 @@
-use crate::terminal::EConfig;
+use crate::terminal::Terminal;
 
-use std::fmt;
-use std::io::Write; // for flush() in clearing screen
-use std::io::{self, stdout};
 use std::process::exit; // for clearer program exit
-                        //
-use termion::cursor;
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
+const E_VERSION: &str = "0.0.1";
+
+// move the screen state to the terminal
 pub struct Editor {
     should_quit: bool,
-    cfg: EConfig,
-    screen_state: Vec<u8>, // the String for screen_state, for a refreshed screen and basic drawing. Write all formatting
-                           // strings
-                           //cursor_pos: cursor::Goto,
-}
-
-struct ABuf {
-    buf: Vec<u8>,
-    len: u16,
-}
-
-impl ABuf {
-    fn append(self, s: &str) {
-        let bytes = s.as_bytes();
-
-        for b in bytes {
-            self.buf.clone().push(*b);
-        }
-    }
-    //fn state_append(mut ptr: *mut [u8], s: &str) {
-    //let bytes = s.as_bytes();
-    //}
+    terminal: Terminal,     // Terminal
+    cursor_pos: (u16, u16), // first is y-axis, second is x-axis?? indexed from 1
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         loop {
             // Handling errors in process keypress
             // if let = matches only condition
@@ -52,7 +25,7 @@ impl Editor {
             }
             if self.should_quit == true {
                 self.refresh_screen().unwrap();
-                _stdout.suspend_raw_mode().unwrap();
+                self.terminal.suspend_raw_mode().unwrap();
                 exit(0)
             }
         }
@@ -60,10 +33,14 @@ impl Editor {
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         //returns error if encountered, does not handle it
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         // Unwrap the result(Key) -> Key
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Char('h') => self.move_cursor(Key::Char('h')),
+            Key::Char('j') => self.move_cursor(Key::Char('j')),
+            Key::Char('k') => self.move_cursor(Key::Char('k')),
+            Key::Char('l') => self.move_cursor(Key::Char('l')),
             _ => (),
         }
         // else returns empty ok, no error returned
@@ -71,48 +48,87 @@ impl Editor {
     }
 
     pub fn default() -> Self {
-        let cnfg = EConfig::default().unwrap();
-        // ptr to buffer for screen_state str
-
+        let cnfg = Terminal::default().unwrap();
         Self {
             should_quit: false,
-            cfg: cnfg,
-            screen_state: Vec::new(),
+            terminal: cnfg,
         }
     }
 
     fn draw_rows(&mut self) {
-        for _y in 0..self.cfg.size.1 - 1 {
-            self.screen_state.append(&mut "~\r\n".as_bytes().to_vec());
+        for y in 0..self.terminal.size.1 - 1 {
+            self.terminal.push_screen_state("\x1b[K"); // clear row to right of cursor
+            if y == self.terminal.size.1 / 3 {
+                // 1/3 down the screen, print the welcome message
+                let ver_string = format!("{} {}\r\n", "Kilo editor -- version", E_VERSION);
+
+                // padding for printing the welcome msg in the middle of the screen
+                let mut padding = (self.terminal.size.0 as usize - ver_string.len()) / 2;
+                if padding > 0 {
+                    self.terminal.push_screen_state("~");
+                    padding -= 1;
+                }
+                for _ in 0..padding {
+                    self.terminal.push_screen_state(" ");
+                }
+                self.terminal.push_screen_state(&ver_string);
+            } else {
+                self.terminal.push_screen_state("~\r\n"); // print tilda and return newline
+            }
         }
     }
 
+    fn move_cursor(&mut self, k: Key) {
+        match k {
+            Key::Char('h') => {
+                if self.cursor_pos.1 > 1 {
+                    self.cursor_pos.1 -= 1
+                }
+            }
+            Key::Char('l') => {
+                if self.cursor_pos.1 < self.terminal.size.0 as usize {
+                    self.cursor_pos.1 += 1
+                }
+            }
+            Key::Char('j') => {
+                if self.cursor_pos.0 < self.terminal.size.1 as usize {
+                    self.cursor_pos.0 += 1
+                }
+            }
+            Key::Char('k') => {
+                if self.cursor_pos.0 > 1 {
+                    self.cursor_pos.0 -= 1
+                }
+            }
+            _ => (),
+        };
+    }
+
+    // using escape sequences (\x1b...), refresh the screen_state
     fn refresh_screen(&mut self) -> Result<(), std::io::Error> {
-        // clear the cursr
-        print!("{}[2J", 27 as char); //27 as char is escape character
-                                     //print!("\x1b[H"); // \x1 is binary escape char; set cursor to top left of the screen
-        print!("\x1b[H");
-        if !self.should_quit {
-            self.draw_rows();
-        }
-        print!("\x1b[H");
-        // must print Gbye after setting cursor at the top
-        if self.should_quit {
-            println!("Gbye :)\r");
-        }
+        self.terminal.push_screen_state("\x1b[?25l"); // turn off cursor
+        self.terminal.push_screen_state("\x1b[H"); // set cursor to top left of the screen
 
-        // flush clears the screen of all previous writing in buffer of terminal?
-        io::stdout().flush()
-    }
-}
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        // locks handle to standard input stream, allowing us to read from it, returns error
-        // if let -> match that handles only one match
-        // if there is a (Some(Result(Key))) -> return Result
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
+        self.draw_rows();
+
+        let cur_string = format!(
+            "\x1b[{};{}H",
+            self.terminal.cursor_pos()?.0,
+            self.terminal.cursor_pos()?.1,
+        );
+        self.terminal.push_screen_state(&cur_string);
+
+        // must print Gbye after setting cursor at the top
+        self.terminal.push_screen_state("\x1b[?25h"); // turn cursor back on
+
+        if self.should_quit {
+            self.terminal.push_screen_state("\x1b[2J"); //clear screen
+            self.terminal.push_screen_state("\x1b[H"); // reset cursor to top left of the screen
+            self.terminal.push_screen_state("Gbye :) \r\n"); // send message
         }
+        self.terminal.write()?;
+        // flush clears the screen of all previous writing in buffer of terminal?
+        self.terminal.flush()
     }
 }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,14 +1,63 @@
+use std::io::{self, stdout, Write};
+
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::terminal_size;
 
-pub struct EConfig {
+pub struct Terminal {
     pub size: (u16, u16),
+    _stdout: RawTerminal<std::io::Stdout>,
+    screen_state: String,
+    cursor_pos: (usize, usize),
 }
 
-impl EConfig {
+impl Terminal {
     pub fn default() -> Result<Self, std::io::Error> {
         let ter_size = terminal_size()?;
         Ok(Self {
             size: (ter_size.0, ter_size.1),
+            _stdout: stdout().into_raw_mode()?,
+            cursor_pos: (1, 1),
+            screen_state: String::new(),
         })
+    }
+
+    pub fn size(&self) -> &(u16, u16) {
+        &self.size
+    }
+
+    pub fn suspend_raw_mode(&self) -> &Result<(), std::io::Error> {
+        &self._stdout.suspend_raw_mode()
+    }
+
+    pub fn push_screen_state(&self, s: &str) -> Result<(), std::io::Error> {
+        Ok(self.screen_state.push_str(s))
+    }
+
+    pub fn write(&mut self) -> Result<usize, std::io::Error> {
+        io::stdout().write(self.screen_state.as_bytes())
+    }
+
+    pub fn flush(&mut self) -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            // locks handle to standard input stream, allowing us to read from it, returns error
+            // if let -> match that handles only one match
+            // if there is a (Some(Result(Key))) -> return Result
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    pub fn cursor_pos(&mut self) -> Result<(usize, usize), std::io::Error> {
+        let x = self.cursor_pos.0.saturating_sub(1);
+        let y = self.cursor_pos.1.saturating_sub(1);
+
+        Ok((x, y))
     }
 }
